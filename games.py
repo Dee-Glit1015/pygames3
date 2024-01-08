202bbe321b34c9b1ecd8f4856de00723ea579c78
Dee-Glit1015 <dedespermatasari25@gmail.com>
2024-01-08 21:10:38 +0800

    Menambahkan Opsi Keluar Games

--- games.py
+++ games.py
@@ -1,56 +1,7 @@
-# Membuat Games Kertas Batu Gunting Kertas
-def main():
-    print("Pilih salah Satu!")
-    print("1.Kertas")
-    print("2.Batu")
-    print("3.Gunting")
-    print("4.Keluar")
-    pilih = int(input("Masukkan Pilihan Anda:"))
-    import random
-    comp = random.randint(1,3)
-    if pilih ==1:
-        if comp == 1:
-            print("Kamu Memilih Kertas")
-            print("Komputer Memilih Kertas")
-            print("Seri!")
-        elif comp == 2:
-            print("Kamu Memilih Kertas")
-            print("Komputer Memilih Batu")
-            print("Komputer Menang!")
-        else:  
-            print("Kamu Memilih Kertas")
-            print("Komputer Memilih Gunting")
-            print("Kamu Menang!")
-    elif pilih == 2:
-        if comp == 1:
-            print("Kamu Memilih Batu")
-            print("Komputer Memilih Kertas")
-            print("Kamu Menang!")
-        elif comp == 2 :
-            print("Kamu Memilih Batu")
-            print("Komputer Mimilih Batu")
-            print("Seri!")
-        else:
-            print("Kamu Memilih Batu")
-            print("Komputer Memilih Gunting")
-            print("Komputer Menang!")
-    elif pilih == 3:
-        if comp == 1:
-            print("Kamu Memilih Gunting") 
-            print("Komputer Memilih Kertas")
-            print("Komputer Menang!")
-        elif comp == 2:
-            print("Kamu Memilih Gunting") 
-            print("Komputer Memilih Batu")
-            print("Kamu Menang!")
-        else:
-            print("Kamu Memilih Gunting")
-            print("Komputer Memilih Gunting")
-            print("Seri!")
-    elif pilih == 4:
-        print("Terima Kasih")
-        exit()
-    else:
-        print("Pilihan Tidak Tersedia")
-
-main()
+-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
+QyNTUxOQAAACCARiSMS4+D9t5uluXWLS0w1gOyMNWSqpSzOZYnubl7YwAAAJAhtiRxIbYk
+cQAAAAtzc2gtZWQyNTUxOQAAACCARiSMS4+D9t5uluXWLS0w1gOyMNWSqpSzOZYnubl7Yw
+AAAECk4EapInInYiZMCo8Nl5khAY+c9cRAXV8WqdrSTo5bKYBGJIxLj4P23m6W5dYtLTDW
+A7Iw1ZKqlLM5lie5uXtjAAAACVVzZXJAQVNVUwECAwQ=
+-----END OPENSSH PRIVATE KEY-----
